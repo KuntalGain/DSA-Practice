a59e187e0e218c0a7b7487f79170d88d69e45e50
Kuntal Gain <101191826+KuntalGain@users.noreply.github.com>
2022-12-16 20:53:32 +0530

    Day 2 done

--- day_2/problem_3.cpp
+++ day_2/problem_3.cpp
@@ -18,3 +18,15 @@ vector<int> twoSum(vector<int>&arr,int target) {
 
     return {};
 }
+
+//Driver code
+
+int main() {
+    vector<int> ans;
+    vector<int> v = {2,7,11,15};
+
+    ans = twoSum(v,9);
+
+    for(auto i : ans)
+        cout << i << " ";
+}

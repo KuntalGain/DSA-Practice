6abec7a538904ba987a375e2d5e66a803455f1a8
Kuntal Gain <101191826+KuntalGain@users.noreply.github.com>
2022-12-28 11:40:40 +0530

    Problem 31 is Solved

--- day_14/problem_31.cpp
+++ day_14/problem_31.cpp
@@ -13,17 +13,18 @@ struct TreeNode
 
 // search in BST
 
-bool isBST(TreeNode* root , int _min , int _max)
-{
-    if(root == nullptr) return true;
-    if(root->val >= _max || root->val <= _max) return false;
-
-    return isBST(root->left,_min,root->val) && isBST(root->left,root->val,_max);
-}
-
 bool isValidBST(TreeNode* root) {
-    return isBST(root,INT_MIN,INT_MAX);    
-}
+        return valid(root, LONG_MIN, LONG_MAX);
+    }
+bool valid(TreeNode* root, long min, long max) {
+        if (!root) {
+            return true;
+        }
+        if (root -> val <= min || root -> val >= max) {
+            return false;
+        }
+        return valid(root -> left, min, root -> val) && valid(root -> right, root -> val, max);
+    }
 
 void preorder(TreeNode* root , vector<int> &Node)
 {
